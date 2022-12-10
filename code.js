8d6307f0257f89f0afb4c622bfee2b5eda3cae3d
will <willdomine1@yahoo.com>
2022-12-10 14:08:31 -0600

    Half-way

--- code.js
+++ code.js
@@ -2,11 +2,13 @@
 
 var data = {
     balance: 500,
-    debt:0,
+    debt:2000,
     limit:0,
     creditScore:0,
-    turn:1,
-    apr:0
+    turn:0,
+    apr:0,
+    payments:0,
+    payIncrease:0
 }
 
 //Text document variables
@@ -22,6 +24,21 @@ var debtTxt = document.getElementById("debt")
 var creditScoreTxt = document.getElementById("credit")
 var limitTxt = document.getElementById("limit")
 var aprTxt = document.getElementById("apr")
+var rulesBtn = document.getElementById("rules")
+var PaybtnContainer =document.getElementById("payBtnContainer")
+var PayDebt = document.getElementById("payDebt")
+var removeDebt = document.getElementById("removeDebt")
+
+PayDebt.onclick = function(){
+    if(data.balance >= parseInt(removeDebt.value)){
+        data.balance -= parseInt(removeDebt.value)
+        data.debt -= parseInt(removeDebt.value)
+        if(data.payments < data.turn){
+            data.payments += 1
+        }
+    }
+    refresh()
+}
 
 function add(key, value){
     data[key] = data[key] + (value || 1)
@@ -36,51 +53,101 @@ function Interest(key, key2){
     data[key] = data[key] * (1+ (data[key2]/100))
 }
 
-function leftClick(){
-a1Btn.onclick() = function(){
+function calcCreditScore(key,key2,key3,key4){
+    data[key] = 850 - ((data[key2] * .3)/(data[key3]+data[key4]))
+}
+
+a1Btn.onclick = function(){
+    console.log("working")
     switch(data.turn){
         case 1:{
             add("limit",10000)
             add("apr", 12)
+            gameManager()
         }
+        break;
+        case 2:{
+            add("debt",3000)
+            add("apr", 2)
+            gameManager()
+        }
+        break;
+        case 3:{
+            add("payIncrease",100)
+            gameManager()
+        }
+        break;
+        case 4:{
+            add("balance",2000)
+            gameManager()
+        }
+        break;
     }
-}
+    
 }
 
-function gameManager(turn){
+
+function gameManager(){
+    add("turn",1)
     switch(data.turn){
         case 1:{
             questionTxt.innerHTML = "Its your first turn choose your credit card"
             a1.innerHTML = "Credit limit of $10,000 and a APR of 12%"
             a2.innerHTML = "Credit limit of $8,000 and a APR of 10%"
-            add("limit",10000)
-            add("apr", 12)
         }
+        break;
         case 2:{
-            questionTxt.innerHTML = ""
+            questionTxt.innerHTML = "You need to get a loan choose"
+            a1.innerHTML = "Car loan for $3,000 but APR will +2%"
+            a2.innerHTML = "Car loan for $1,000 but APR will +4%"
+        }
+        break;
+        case 3:{
+            questionTxt.innerHTML = "Sense you got a car loan you've worked harder and got a raise and offer"
+            a1.innerHTML = "Take Offer make $600 per turn"
+            a2.innerHTML = "Take Raise 50% chance of $550 or $650"
+        }
+        break;
+        case 4:{
+            questionTxt.innerHTML = "You find a brief case with $2,000"
+            a1.innerHTML = "Keep the $2000"
+            a2.innerHTML = "Turn it in for 50% chance of getting $500 or $2,500 reward"
         }
     }
+    add("balance",(500+data.payIncrease))
     Interest("debt","apr")
-    add("turn",1)
+    if(data.turn % 2 == 0){
+        calcCreditScore("creditScore","debt","turn","payments")
+    }
     refresh()
 }
 
-try{
-    startBtn.onclick = function(){
-        open("game1.html","_self")
-        
-    }
-}
-catch(error){
-    refresh()
+
+function startGame(){
+    open("game1.html","_self")
 }
 
 function refresh(){
     turnTxt.innerHTML = 'Turn: ' + data.turn
-    balanceTxt.innerHTML = "Balance: " +data.balance
-    debtTxt.innerHTML = "Debt: " + data.debt
-    creditScoreTxt.innerHTML = "Credit Score: " + data.creditScore
-    limitTxt.innerHTML = "Limit: " + data.limit
-    aprTxt.innerHTML = "APR: " + data.apr + "%"
+    balanceTxt.innerHTML = "Balance: $" +data.balance.toLocaleString()
+    debtTxt.innerHTML = "Debt: $" + data.debt.toLocaleString()
+    creditScoreTxt.innerHTML = "Credit Score: " + Math.round(data.creditScore) 
+    limitTxt.innerHTML = "Limit: $" + data.limit.toLocaleString()
+    aprTxt.innerHTML = "APR: " + data.apr.toLocaleString() + "%"
 }
-gameManager(data.turn)
+
+rulesBtn.onclick = function(){
+    data = {
+        balance: 500,
+        debt:2000,
+        limit:0,
+        creditScore:0,
+        turn:0,
+        apr:0,
+        payments:0
+    }
+    removeDebt.value = ""
+    gameManager()
+    refresh()
+
+}
