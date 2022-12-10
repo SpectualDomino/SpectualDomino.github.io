b776f33b65d71040aa7c9807314a81ac070e05d8
will <willdomine1@yahoo.com>
2022-12-10 11:42:05 -0600

    Going good

--- code.js
+++ code.js
@@ -1,66 +1,86 @@
+//Main vars for game
 
-function chooseDifficulty(bal,debt, creditScore,missedPayment){
-    localStorage.setItem("bal", bal)
-    localStorage.setItem("debt", debt)
-    localStorage.setItem("creditScore", creditScore)
-    localStorage.setItem("missedPayment",missedPayment)
-    localStorage.setItem("limit", 5000)
-    localStorage.setItem("apr", 0)
-    open("game1.html","_self")
+var data = {
+    balance: 500,
+    debt:0,
+    limit:0,
+    creditScore:0,
+    turn:1,
+    apr:0
 }
-function updateBal(){
-    var lBal = localStorage.getItem("bal")
-    var lDebt = localStorage.getItem("debt")
-    var lCreditScore = localStorage.getItem("creditScore")
-    var lmissedPayment = localStorage.getItem("missedPayment")
 
-    var lApr = localStorage.getItem("apr")
+//Text document variables
+var startBtn = document.getElementById("start")
+var questionTxt = document.getElementById("question")
+var a1 = document.getElementById("a1")
+var a2 = document.getElementById("a2")
+var a1Btn = document.getElementById("a1Btn")
+var a2Btn = document.getElementById("a2Btn")
+var turnTxt = document.getElementById("turnTxt")
+var balanceTxt = document.getElementById("balance")
+var debtTxt = document.getElementById("debt")
+var creditScoreTxt = document.getElementById("credit")
+var limitTxt = document.getElementById("limit")
+var aprTxt = document.getElementById("apr")
 
-    var balance = document.getElementById("balance")
-    var debt = document.getElementById("money")
-    var credit = document.getElementById("credit")
-    var missedP = document.getElementById("missedP")
-
-    var apr = document.getElementById("apr")
-
-    balance.innerHTML = "Current Balance: $" + lBal
-    debt.innerText = "Current Debt: $"+ lDebt
-    credit.innerHTML = "Current Credit Score: " + lCreditScore
-    missedP.innerHTML = "Missed Payments: " + lmissedPayment
-    apr.innerHTML = "Current Apr: " + lApr + "%"
-
-    
-    
+function add(key, value){
+    data[key] = data[key] + (value || 1)
 }
-function userChoice(newBal,newDebt,newCredit,newMissedPayment,newLimit,newApr){
-    var lBal = localStorage.getItem("bal")
-    var lDebt = localStorage.getItem("debt")
-    var lCreditScore = localStorage.getItem("creditScore")
-    var lLimit = localStorage.getItem("limit")
-    var lmissedPayment = localStorage.getItem("missedPayment")
-    var lTurn = localStorage.getItem("turn")
-    var lApr = localStorage.getItem("apr")
-    
-    localStorage.setItem("bal",lBal += newBal)
-    localStorage.setItem("debt", lDebt += newDebt)
-    localStorage.setItem("creditScore", lCreditScore += newCredit)
-    localStorage.setItem("limit", lLimit += newLimit)
-    localStorage.setItem("missedPayment", lmissedPayment += newMissedPayment)
-    localStorage.setItem("turn", lTurn += 1)
-    localStorage.setItem("apr",lApr += newApr)
-    updateBal()
-}
-function carLoan(amount,turns){
-    var lBal = localStorage.getItem("bal")
-    var lDebt = localStorage.getItem("debt")
-    var lCreditScore = localStorage.getItem("creditScore")
-    var lLimit = localStorage.getItem("limit")
-    var lmissedPayment = localStorage.getItem("missedPayment")
-    var lTurn = localStorage.getItem("turn")
-    var lApr = localStorage.getItem("apr")
 
-    if(turns > 0){
-    localStorage.setItem("")
+function subtract(key=String,value){
+    data[key] = data[key] - (value || 1)
+
+}
+
+function Interest(key, key2){
+    data[key] = data[key] * (1+ (data[key2]/100))
+}
+
+function leftClick(){
+a1Btn.onclick() = function(){
+    switch(data.turn){
+        case 1:{
+            add("limit",10000)
+            add("apr", 12)
+        }
     }
-
 }
+}
+
+function gameManager(turn){
+    switch(data.turn){
+        case 1:{
+            questionTxt.innerHTML = "Its your first turn choose your credit card"
+            a1.innerHTML = "Credit limit of $10,000 and a APR of 12%"
+            a2.innerHTML = "Credit limit of $8,000 and a APR of 10%"
+            add("limit",10000)
+            add("apr", 12)
+        }
+        case 2:{
+            questionTxt.innerHTML = ""
+        }
+    }
+    Interest("debt","apr")
+    add("turn",1)
+    refresh()
+}
+
+try{
+    startBtn.onclick = function(){
+        open("game1.html","_self")
+        
+    }
+}
+catch(error){
+    refresh()
+}
+
+function refresh(){
+    turnTxt.innerHTML = 'Turn: ' + data.turn
+    balanceTxt.innerHTML = "Balance: " +data.balance
+    debtTxt.innerHTML = "Debt: " + data.debt
+    creditScoreTxt.innerHTML = "Credit Score: " + data.creditScore
+    limitTxt.innerHTML = "Limit: " + data.limit
+    aprTxt.innerHTML = "APR: " + data.apr + "%"
+}
+gameManager(data.turn)
